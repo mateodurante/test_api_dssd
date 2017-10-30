a44fd275ebe01b83fcbd1b731a7abd8be8a3cf6f
Mateo Durante <mdurante@cert.unlp.edu.ar>
2017-10-30 15:02:27 -0300

    asdf

--- api.py
+++ api.py
@@ -1,12 +1,14 @@
 #!flask/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import datetime
 
 app = Flask(__name__)
 
 incidentes = [
     {
+        'idIncidente': 1,
         'nroCliente': 1,
+        'estado': 1,
         #'tipoIncidente': '',
         'descripcionIncidente': "Me rompieron el rancho",
         'objetos': [
@@ -25,7 +27,9 @@ incidentes = [
         'fechaIncidente': str(datetime.datetime.now().strftime("%Y/%m/%d"))
     },
     {
+        'idIncidente': 2,
         'nroCliente': 4,
+        'estado': 1,
         #'tipoIncidente': '',
         'descripcionIncidente': "Me entraron a la choza",
         'objetos': [
@@ -49,6 +53,18 @@ incidentes = [
 def get_tasks():
     #return jsonify({'incidentes': incidentes})
     return jsonify(incidentes[1])
+
+@app.route('/api/incidente/cambioEstado/recibido', methods=['POST'])
+def post_incident_status():
+    data = request.form
+    inc = None
+    print(data)
+    #for i in incidentes:
+    #    if i['idIncidente'] == post_id:
+    #        i['estado'] == 'recibido'
+    return 1
+    #return jsonify({'incidentes': incidentes})
+    #return jsonify(incidentes[1])
 
 if __name__ == '__main__':
     app.run(debug=True)
